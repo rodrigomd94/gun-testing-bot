55591557ad7f712fa87c76434545d593d4e364d0
rodrigomd94 <rodrigomd94@gmail.com>
2022-04-02 09:41:50 -0600

    prepended cardano and testnet nodes

--- bot.mjs
+++ bot.mjs
@@ -5,6 +5,8 @@ const gun = new Gun(hosts)
 const randomStrings = ["Random string", null, "Lorem ipsum dolor sit amet,", "consectetur adipiscing", "elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "Ut enim ad minim veniam, quis nostrud exercitation ullamco", "laboris nisi ut aliquip ex ea commodo consequat.", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum", "dolore eu fugiat nulla pariatur.", "Excepteur sint occaecat cupidatat non proident,", "sunt in culpa qui officia deserunt mollit anim id", "est laborum"]
 
 const transactionsNode = gun
+  .get('cardano')
+  .get('testnet')
   .get('transactions')
 
 var signersList = [];
